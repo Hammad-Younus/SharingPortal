fc1d293a86f8664e1394a17df2cf6acee2414d29
hammad <hammad@gmail.com>
2016-09-26 22:20:54 +0500

    making of home page

---
+++ application/views/includes/header.php
@@ -0,0 +1,16 @@
+<!--         <div class="banner-top">
+            <img alt='top banner' src="http://placehold.it/1600x90">
+        </div> -->
+
+        <header class="header-wrapper header-wrapper--home">
+            <div class="container">
+                <!-- Logo link-->
+                <a href='index.html' class="logo">
+                    <img alt='logo' src="<?php echo base_url();?>assets/images/logo.png">
+                </a>
+                
+                <!-- Main website navigation-->
+                <?php $this->load->view('includes/navs');?>
+
+            </div>
+        </header>
